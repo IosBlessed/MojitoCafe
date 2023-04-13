bb7a462161506b16ff7763de5e28040211b10ac6
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-04-13 18:34:13 +0000

    Resctructured logic of Category Model (extended with Porduct state)

--- MojitoCafe/Screens/Home/Model/ProductModel.swift
+++ MojitoCafe/Screens/Home/Model/ProductModel.swift
@@ -4,14 +4,19 @@
 //
 //  Created by Никита Данилович on 08.04.2023.
 //
+
+
 struct Category{
     
     var id:Int
     var name:String
     
-    init(name: String, id:Int) {
+    var products:[Product]?
+    
+    init(name: String, id:Int, products:[Product]? = nil) {
         self.id = id
         self.name = name
+        self.products = products
     }
 
 }
@@ -22,14 +27,16 @@ struct Product{
     var title:String
     var description:String
     var price:Int
+    var currency:String
     var imagePath:String
     var categoryID:Int
     
-    init(id:Int,title: String, description: String, price: Int, imagePath: String, categoryID:Int) {
+    init(id:Int,title: String, description: String, price: Int, currency:String, imagePath: String, categoryID:Int) {
         self.id = id
         self.title = title
         self.description = description
         self.price = price
+        self.currency = currency
         self.imagePath = imagePath
         self.categoryID = categoryID
     }
