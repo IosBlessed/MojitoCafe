79bc79bd0e16e05944679b24b49a4c0ba7188885
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-04-13 18:41:20 +0000

    Additional functionality for products extraction from firebase

--- MojitoCafe/Screens/Home/API's/ServiceDatabase.swift
+++ MojitoCafe/Screens/Home/API's/ServiceDatabase.swift
@@ -45,11 +45,62 @@ class APIDatabase{
                     let categoryID = document["id"] as? Int ?? 0
                     let categoryName = document["name"] as? String ?? "Unknown category"
                     
-                    return Category(name: categoryName, id: categoryID )
+                    return Category(name: categoryName, id: categoryID)
                 }
                 return completion(categories,nil)
             }
         }
+    }
+    
+    func getProductsFromFirestore(collection:String, completion:@escaping([Product]?,Error?) -> Void){
+        
+        let firestore = Firestore.firestore()
+        
+        firestore.collection(collection).getDocuments{
+            QuerySnapshot, error in
+            
+            guard error == nil else {
+                completion(nil,error)
+                return
+            }
+            
+            let products = QuerySnapshot?.documents.map{
+                document in
+                
+                document.data()
+            }
+            
+            
+            if let products = products{
+                
+                let productsArray:[Product] = products.map{
+                    product in
+                    let id = product["id"] as? Int ?? -1
+                    let title = product["name"] as? String ?? "Unknown"
+                    let categoryID = product["categoryID"] as? Int ?? -1
+                    let currency = product["currency"] as? String ?? "Unknown"
+                    let description = product["description"] as? String ?? "Unknown"
+                    let price = product["price"] as? Int ?? -100
+                    let imagePath = product["imagePath"] as? String ?? "button.programmable"
+                    
+                    return Product(
+                        id: id,
+                        title: title,
+                        description: description,
+                        price: price,
+                        currency: currency,
+                        imagePath: imagePath,
+                        categoryID: categoryID
+                    )
+                }
+                
+                completion(productsArray,nil)
+                
+            }
+            
+            
+        }
+        
     }
     
     
